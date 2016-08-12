0fee309f319ad6b9fd9cd8b272b12c1d39d2b6f2
Zaheer Sheikh <zaheer_sheikh@persistent.co.in>
2016-08-12 18:15:10 +0530

    created test unit

--- classes/tests/userTest.php
+++ classes/tests/userTest.php
@@ -1,22 +1,23 @@
 <?php
 //require dirname(__FILE__) . 'user.php';
-require '/var/lib/jenkins/workspace/CaseStudyBuildAnt/classes/user.php';
+require_once('/var/lib/jenkins/workspace/CaseStudyBuildAnt/includes/config.php');
+//require '/var/lib/jenkins/workspace/CaseStudyBuildAnt/classes/user.php';
 class UserTest extends PHPUnit_Framework_TestCase{
 
 	private $testValue;
 
     public function setUp()
     {
-        $this->testValue = new User();
+        //$this->testValue = new User();
     }
 	public function testRegistrationLogin()
     {
-        $this->testValue->setGumballs(100);
-		$this->assertEquals(99, $this->testValue->turnWheel(100));
+        $user->setGumballs(100);
+		$this->assertEquals(99, $user->turnWheel(100));
     }
 	public function testLogin()
     {
-        $this->assertEquals(99, $this->testValue->turnWheel(100));
+        $this->assertEquals(99, $user->turnWheel(100));
     }
 	
 
