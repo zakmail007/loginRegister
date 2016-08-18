f0473fdecbc681b3b90dd15cfe2dce7388da84d9
Zaheer Sheikh <zaheer_sheikh@persistent.co.in>
2016-08-18 17:23:46 +0530

    devOpsCaseStudy deployment

--- classes/tests/userTest.php
+++ classes/tests/userTest.php
@@ -15,9 +15,11 @@ class UserTest extends PHPUnit_Framework_TestCase{
         $this->testValue->setGumballs(100);
 		$this->testValue->turnWheel(100);
 		$this->assertEquals(99, $this->testValue->turnWheel(100));
-		$this->testValue->passLength(10);
+    }
+	public function testPassLength()
+    {
+        $this->testValue->passLength('zr');
 		$this->assertTrue($this->testValue->passLength('zx'));
-
     }
 }
 ?>
